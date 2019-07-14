79552a1508ecb079eea356db9aee9a107bcfc68d
Mark Frymire <mark.frymire@icloud.com>
2019-07-14 15:32:23 -0400

    Refactor OLEDGraphicsTest

--- OLEDGraphicsTest/OLEDGraphicsTest/OLEDGraphicsTest.ino
+++ OLEDGraphicsTest/OLEDGraphicsTest/OLEDGraphicsTest.ino
@@ -19,6 +19,42 @@ const int csPin = 6;
 const int dcPin = 7;
 U8GLIB_SH1106_128X64 u8g(clkPin, mosiPin, csPin, dcPin);
 
+uint8_t draw_state = 0;
+
+void setup(void) {
+  u8g.setRot180(); // flip screen, if required
+}
+
+void loop(void) {
+  
+  // picture loop  
+  u8g.firstPage();  
+  do {
+    draw();
+  } while(u8g.nextPage());
+  
+  // increase the state
+  draw_state++;
+  if (draw_state >= 9*8) draw_state = 0;
+  
+  delay(150);
+}
+
+void draw(void) {
+  u8g_prepare();
+  switch(draw_state >> 3) {
+    case 0: u8g_box_frame(draw_state&7); break;
+    case 1: u8g_disc_circle(draw_state&7); break;
+    case 2: u8g_r_frame(draw_state&7); break;
+    case 3: u8g_string(draw_state&7); break;
+    case 4: u8g_line(draw_state&7); break;
+    case 5: u8g_triangle(draw_state&7); break;
+    case 6: u8g_ascii_1(); break;
+    case 7: u8g_ascii_2(); break;
+    case 8: u8g_extra_page(draw_state&7); break;
+  }
+}
+
 void u8g_prepare(void) {
   u8g.setFont(u8g_font_6x10);
   u8g.setFontRefHeightExtendedText();
@@ -39,13 +75,13 @@ void u8g_disc_circle(uint8_t a) {
   u8g.drawStr(0, 0, "drawDisc");
   u8g.drawDisc(10, 18, 9);
   u8g.drawDisc(24 + a, 16, 7);
-  u8g.drawStr( 0, 30, "drawCircle");
+  u8g.drawStr(0, 30, "drawCircle");
   u8g.drawCircle(10, 18 + 30, 9);
   u8g.drawCircle(24 + a, 16 + 30, 7);
 }
 
 void u8g_r_frame(uint8_t a) {
-  u8g.drawStr( 0, 0, "drawRFrame/Box");
+  u8g.drawStr(0, 0, "drawRFrame/Box");
   u8g.drawRFrame(5, 10, 40, 30, a + 1);
   u8g.drawRBox(50, 10, 25, 40, a + 1);
 }
@@ -98,78 +134,36 @@ void u8g_ascii_2() {
   }
 }
 
-void u8g_extra_page(uint8_t a)
-{
-  if (u8g.getMode() == U8G_MODE_HICOLOR || u8g.getMode() == U8G_MODE_R3G3B2) {
-    
-    /* draw background (area is 128x128) */
-    u8g_uint_t r, g, b;
-    b = a << 5;
-    for(g = 0; g < 64; g++) {
-      for(r = 0; r < 64; r++) {
-      	u8g.setRGB(r<<2, g<<2, b );
-	      u8g.drawPixel(g, r);
-      }
-    }
-    u8g.setRGB(255, 255, 255);
-    u8g.drawStr(66, 0, "Color Page");
-    
-  } else if (u8g.getMode() == U8G_MODE_GRAY2BIT) {
-    u8g.drawStr(66, 0, "Gray Level");
-    u8g.setColorIndex(1);
-    u8g.drawBox(0, 4, 64, 32);    
-    u8g.drawBox(70, 20, 4, 12);
-    u8g.setColorIndex(2);
-    u8g.drawBox(0 + 1*a, 4 + 1*a, 64 - 2*a, 32 - 2*a);
-    u8g.drawBox(74, 20, 4, 12);
-    u8g.setColorIndex(3);
-    u8g.drawBox(0 + 2*a, 4 + 2*a, 64 - 4*a, 32 - 4*a);
-    u8g.drawBox(78, 20, 4, 12);
-  } else {
-    u8g.drawStr(0, 12, "setScale2x2");
+void u8g_extra_page(uint8_t a) {
+//  if (u8g.getMode() == U8G_MODE_HICOLOR || u8g.getMode() == U8G_MODE_R3G3B2) {
+//    
+//    /* draw background (area is 128x128) */
+//    u8g_uint_t r, g, b;
+//    b = a << 5;
+//    for(g = 0; g < 64; g++) {
+//      for(r = 0; r < 64; r++) {
+//        u8g.setRGB(r << 2, g << 2, b);
+//        u8g.drawPixel(g, r);
+//      }
+//    }
+//    u8g.setRGB(255, 255, 255);
+//    u8g.drawStr(66, 0, "Color Page");
+//    
+//  } else if (u8g.getMode() == U8G_MODE_GRAY2BIT) {
+//    u8g.drawStr(66, 0, "Gray Level");
+//    u8g.setColorIndex(1);
+//    u8g.drawBox(0, 4, 64, 32);
+//    u8g.drawBox(70, 20, 4, 12);
+//    u8g.setColorIndex(2);
+//    u8g.drawBox(0 + 1*a, 4 + 1*a, 64 - 2*a, 32 - 2*a);
+//    u8g.drawBox(74, 20, 4, 12);
+//    u8g.setColorIndex(3);
+//    u8g.drawBox(0 + 2*a, 4 + 2*a, 64 - 4*a, 32 - 4*a);
+//    u8g.drawBox(78, 20, 4, 12);
+//  } else {
+    u8g.drawStr(0, 12, "setScale1x1");
     u8g.setScale2x2();
-    u8g.drawStr(0, 6+a, "setScale2x2");
+    u8g.drawStr(0, 6 + a, "setScale2x2");
     u8g.undoScale();
-  }
-}
-
-
-uint8_t draw_state = 0;
-
-void draw(void) {
-  u8g_prepare();
-  switch(draw_state >> 3) {
-    case 0: u8g_box_frame(draw_state&7); break;
-    case 1: u8g_disc_circle(draw_state&7); break;
-    case 2: u8g_r_frame(draw_state&7); break;
-    case 3: u8g_string(draw_state&7); break;
-    case 4: u8g_line(draw_state&7); break;
-    case 5: u8g_triangle(draw_state&7); break;
-    case 6: u8g_ascii_1(); break;
-    case 7: u8g_ascii_2(); break;
-    case 8: u8g_extra_page(draw_state&7); break;
-  }
-}
-
-void setup(void) {
-  u8g.setRot180(); // flip screen, if required
-  pinMode(13, OUTPUT);           
-  digitalWrite(13, HIGH);  
-}
-
-void loop(void) {
-  
-  // picture loop  
-  u8g.firstPage();  
-  do {
-    draw();
-  } while( u8g.nextPage() );
-  
-  // increase the state
-  draw_state++;
-  if (draw_state >= 9*8)
-    draw_state = 0;
-  
-  // rebuild the picture after some delay
-  //delay(150);
+//  }
 }
