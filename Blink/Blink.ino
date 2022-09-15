0e9c9cb8e6302442579bfce3ebcce5ebd039b8a2
Mark Frymire <mark.frymire@icloud.com>
2022-09-14 23:17:38 -0400

    Refactor the Blink test code

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -22,16 +22,18 @@
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
+//const int LED_PIN = LED_BUILTIN;
+const int LED_PIN = 8; // bottom left pin on ATmega328p DIP package
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT); // initialize digital pin LED_PIN as an output
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                    // wait for 0.1 s
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                    // wait for 0.1 s
 }
